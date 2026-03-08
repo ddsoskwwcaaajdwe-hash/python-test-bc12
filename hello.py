597750b250114bb41e90f0d4f2ab03c004ae1d09
Jui <ddsoskwwcaaajdwe@gmail.com>
2026-03-08 12:16:11 +0000

    add new functio

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 ##this is my first script
 print("hello world")
 print("i'm using github")
+print("it's easy")
 
+def hello():
+  name = input("What's your name: ")
+  print(f"Hello {name}!")
+  
+#test function
+hello()
